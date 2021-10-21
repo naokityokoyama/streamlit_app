bdbf30ee2d1163f76e0ab99994be0fca89f66947
naoki yokoyama <naoki.yokoyama@eemovel.com>
2021-10-21 15:16:32 -0300

    first

---
+++ similar.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from haversine import haversine
+from sklearn.metrics.pairwise import euclidean_distances, haversine_distances, manhattan_distances
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import warnings
+warnings.filterwarnings("ignore")
+
+def haver(dataset, num):
+    lista_haver = []
+    distancia1 = [dataset.loc[num]['original_address_latitude'], dataset.loc[num]['original_address_longitude']]
+    
+    for i in range(dataset.shape[0]):
+        lista_haver.append(haversine(distancia1, (dataset.loc[i]['original_address_latitude'], dataset.loc[i]['original_address_longitude'])))
+    return lista_haver  
+
+def euclidian(dataset, num):
+    lista_euclidiana = []
+    id = dataset.index
+    min = MinMaxScaler()
+    dataset = min.fit_transform(dataset)
+    dataset = pd.DataFrame(dataset).set_index(id)
+    for index, row in dataset.iterrows():
+        X = dataset.loc[num]
+        lista_euclidiana.append(euclidean_distances([X],[row.values]))
+    lista_euclidiana_ = [x[0][0].round(2) for x in lista_euclidiana] 
+    return lista_euclidiana_
+
+def manhattan(dataset, num):
+    lista_manhattan = []
+    for index, row in dataset.iterrows():
+        X = dataset.loc[num]
+        lista_manhattan.append(manhattan_distances([X],[row.values]))
+    lista_manhattan_ = [x[0][0].round(2) for x in lista_manhattan] 
+    return lista_manhattan_
+
+def similar(dataset , num, s=None, raio=200, similar=0.5):
+    '''  
+        dataset = dataset a ser pesquisado,
+        num = amostra,
+        s=None retorna dataset completo s=1 retorna dataset somente com a distancia,
+        raio = 200 default,
+        similar = 0.5 default,
+    '''
+    lista_haver = haver(dataset, num)
+
+    dataset['haver'] = lista_haver
+    query = dataset['haver']<= raio/800
+    data = dataset[query][['features_bedroom', 'total_area', 'features_garage', 'haver']]
+    
+    if s == 1:
+        return data
+    else:    
+        data['euclidian'] = euclidian(data, num)
+        dataset = dataset.loc[data.index]
+        dataset['euclidian'] = data['euclidian']
+        
+        data['manhattan'] = manhattan(data, num)
+        dataset = dataset.loc[data.index]
+        dataset['manhattan'] = data['manhattan']
+        return dataset[dataset['euclidian']<=similar].sort_values(by='euclidian')    
---
+++ stream.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import folium
+from streamlit_folium import folium_static
+from folium.plugins import MarkerCluster
+
+from similar import *
+
+st.title('Teste Similaridade')
+df = pd.read_csv('base_nit.csv')
+
+query = (df['features_bedroom']==0) & \
+        (df['features_bathroom']==0)
+df = df.drop(df[query].index).reset_index(drop=True)    
+
+similaridade = [0.5, 0.1, 0.2, 0.3, 0.4, 1, 1.5]
+
+st.sidebar.header('Filtros')
+selecione_amostra = st.sidebar.selectbox('Selecione Amostra', range(0,200))
+
+amostra = df.loc[selecione_amostra]
+localizacao = df[['original_address_latitude','original_address_longitude']].values.tolist()
+
+selecione_similaridade = st.sidebar.selectbox('Selecione Similaridade', similaridade)
+
+data = similar(df, selecione_amostra, similar=selecione_similaridade)
+#checkbox
+carregar_dados = st.sidebar.checkbox('Carregar dados')
+
+if carregar_dados:
+        st.subheader('Dados')
+        dados=st.dataframe(data)
+
+#st.dataframe(data)
+
+map = folium.Map(location=[df['original_address_latitude'].mean(), df['original_address_longitude'].mean()],defaul_zoom_start=10)
+make_cluster = MarkerCluster().add_to(map)
+
+for index, row in data.iterrows():
+    folium.Marker([row['original_address_latitude'], row['original_address_longitude']], 
+    popup='Preço R$ {0}, Metragem {1}, Quartos {2}'.format(
+        row['transaction_sale'], row['total_area'], row['features_bedroom'])).add_to(make_cluster)
+    
+folium_static(map)    
+
+
+
